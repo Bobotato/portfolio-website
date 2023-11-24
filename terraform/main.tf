31ba94fc4472d019be1f49b273845cfb30eca598
Bobotato <bhojwanialexander@gmail.com>
2023-11-24 09:22:40 +0800

    Update terraform config

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
-resource "digitalocean_droplet" "spendtrack" {
+resource "digitalocean_droplet" "portfolio" {
   count = 1
   image = "debian-11-x64"
-  name = "spendtrack-${count.index}"
+  name = "portfolio-${count.index}"
   region = "sgp1"
   size = "s-1vcpu-512mb-10gb"
 
@@ -18,14 +18,14 @@ resource "digitalocean_droplet" "spendtrack" {
   }
 
     provisioner "local-exec" {
-                command = "until nc -zv '${self.ipv4_address}' 22; do sleep 1; done; echo 'SSH port open' && ansible-playbook -u root -i '${self.ipv4_address},' --private-key ${var.pvt_key} -e @/home/alex/Desktop/hackathon-project/terraform/dockerhub_login.yml -e @/home/alex/Desktop/hackathon-project/terraform/database_details.yml /home/alex/Desktop/hackathon-project/playbook.yml"
+                command = "until nc -zv '${self.ipv4_address}' 22; do sleep 1; done; echo 'SSH port open' && ansible-playbook -u root -i '${self.ipv4_address},' --private-key ${var.pvt_key} -e @/home/alex/Desktop/portfolio-website/terraform/dockerhub_login.yml /home/alex/Desktop/portfolio-website/playbook.yml"
 
     }
 }
 
 output "drop_ip_addresses" {
     value = {
-        for droplet in digitalocean_droplet.spendtrack:
+        for droplet in digitalocean_droplet.portfolio:
         droplet.name => droplet.ipv4_address
     }
 }
