0171185f5f686919dc375efda25b8ff9ad264cf0
Bobotato <bhojwanialexander@gmail.com>
2024-01-05 02:06:54 +0800

    feat: Fix formatting for about page

--- frontend/src/components/LandingPage3D.vue
+++ frontend/src/components/LandingPage3D.vue
@@ -8,61 +8,123 @@ import LoadingSpinner from '@/components/LoadingSpinner.vue'
 
 import { loadGLTFModel } from '@/utils/modelLoader'
 
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+
+// let isLoading = ref(true)
+
+// const rex3d = ref<HTMLCanvasElement | null>(null)
+
+// const scene = new Scene();
+
+// function createCamera() {
+//   const camera = new PerspectiveCamera(20, 1, 0.1, 1000);
+//   camera.position.set(1, 2, 10);
+//   return camera
+// }
+
+// async function loadRex() {
+//   const renderer = new WebGLRenderer({
+//     canvas: rex3d.value as unknown as HTMLCanvasElement,
+//     antialias: true,
+//     });
+
+//   let camera = createCamera()
+
+//   let rex = await loadGLTFModel(scene, '/src/assets/3DModels/rexModel.gltf');
+
+//   renderer.setPixelRatio(window.devicePixelRatio);
+//   renderer.setClearColor( 0xffffff, 0);
+
+//   renderer.render( scene, camera );
+
+//   const pointLight = new PointLight(0xffffff, 50);
+//   pointLight.position.set(3,3,3);
+//   scene.add(pointLight);
+
+//   const controls = new OrbitControls(camera, renderer.domElement);
+//   controls.enablePan = false;
+//   controls.enableZoom = false;
+
+//   function animate() {
+//     // if(model)
+//     //   model.rotation.y += 0.01
+//   requestAnimationFrame(animate);
+//   renderer.render(scene, camera);
+//   }
+  
+//   controls.update;
+//   isLoading.value = false
+
+//   animate()
+// }
+
+// onMounted(() => {
+//   createCamera()
+//   loadRex()
+// })
 
 let isLoading = ref(true)
 
 const rex3d = ref<HTMLCanvasElement | null>(null)
 
+const loader = new GLTFLoader();
+
 const scene = new Scene();
 
-function createCamera() {
-  const camera = new PerspectiveCamera(20, 1, 0.1, 1000);
-  camera.position.set(1, 2, 10);
-}
+const camera = new PerspectiveCamera(20, 1, 0.1, 1000);
 
-async function loadRex() {
-  const renderer = new WebGLRenderer({
+onMounted(() => {
+
+    const renderer = new WebGLRenderer({
     canvas: rex3d.value as unknown as HTMLCanvasElement,
     antialias: true,
     });
+    
+    let rex;
 
-  let rex: any;
+    loader.load('/src/assets/3DModels/rexModel.gltf', function(gltf) {
+      const model = gltf.scene;
+      scene.add(model);
+      rex = model;
+      model.position.setY(-0.15);
+      isLoading.value = false;
+      })
 
-  const gltf = await loadGLTFModel(scene, '/src/assets/3DModels/rexModel.gltf');
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setClearColor( 0xffffff, 0);
+    renderer.setSize(400,400);
+    camera.position.setZ(10);
+    camera.position.setY(2);
+    camera.position.setX(1);
 
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setClearColor( 0xffffff, 0);
+    renderer.render( scene, camera );
 
-  renderer.render( scene, camera );
+    const pointLight = new PointLight(0xffffff, 50);
+    pointLight.position.set(3,3,3);
+    scene.add(pointLight);
 
-  const pointLight = new PointLight(0xffffff, 50);
-  pointLight.position.set(3,3,3);
-  scene.add(pointLight);
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.enablePan = false;
+    controls.enableZoom = false;
 
-  const controls = new OrbitControls(camera, renderer.domElement);
-  controls.enablePan = false;
-  controls.enableZoom = false;
+    function animate() {
+      if(rex)
+        rex.rotation.y += 0.01
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
 
-  function animate() {
-    if(rex)
-      rex.rotation.y += 0.01
-  requestAnimationFrame(animate);
-  renderer.render(scene, camera);
-  }
-  
-  controls.update;
-  isLoading.value = false
+    controls.update;
+    }
 
-  animate()
-}
+    animate()
 
-onMounted(() => {
-  createCamera()
-  loadRex()
 })
 </script>
 
 <template>
-<LoadingSpinner class="absolute left-64 top-48 z-10" v-if="isLoading"></LoadingSpinner>
-<canvas ref="rex3d"></canvas>
+<div v-if=isLoading class="flex w-[400px] h-[400px] items-center justify-center">
+  <LoadingSpinner></LoadingSpinner>
+</div>
+  <canvas v-show="!isLoading" ref="rex3d"></canvas>
 </template>
--- frontend/src/utils/modelLoader.ts
+++ frontend/src/utils/modelLoader.ts
@@ -1,32 +1,17 @@
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-import type { Scene, Mesh } from 'three';
+import type { Scene } from 'three';
 
 export async function loadGLTFModel(scene: Scene, modelPath: string) {
     const loader = new GLTFLoader();
 
-    loader.load(modelPath, function(gltf) {
-        let model;
+    loader.loadAsync(modelPath, function(gltf) {
 
-        gltf.scene.traverse(function (child) {
-            if ((child as Mesh).isMesh) {
-                const m = child as Mesh
-                m.receiveShadow = true
-                m.castShadow = true
-            }
+        const model = gltf.scene;
+        scene.add(model);
+        model.position.setY(-0.1)
+        return model
         })
-        scene.add(gltf.scene); 
-        }, 
-
-        function ( xhr ) {
-            console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-        },
-          
-        function(error: any) {
-            console.log("Model failed to load")
-            console.log(error)
-        }
-        )
    
 };
 
