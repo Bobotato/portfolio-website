2fe4784b3f346b09c93acbb9388b462bb2e11487
Bobotato <bhojwanialexander@gmail.com>
2024-07-02 19:02:30 +0800

    feat: Minor adjustments to 404 page

--- frontend/src/views/NotFoundView.vue
+++ frontend/src/views/NotFoundView.vue
@@ -5,12 +5,16 @@ import Footer from '@/components/utils/footer/Footer.vue'
 </script>
 
 <template>
-  <div class="flex flex-col mx-auto items-center justify-center w-screen overflow-hidden">
-    <h1 class="text-4xl dark:text-white">Not found.</h1>
-    <p class="mt-2 dark:text-white">There's nothing here.</p>
+  <div class="flex flex-col mx-auto items-center justify-center w-screen h-screen overflow-hidden py-10">
+    <div class="flex flex-col items-center justify-center mt-10">
+      <h1 class="text-4xl font-bold dark:text-white">Not found.</h1>
+      <p class="mt-2 dark:text-white">There's nothing here.</p>
+    </div>
     <WalkingAnim></WalkingAnim>
-   
-    <RouterLink to="/" class="hover:text-green-700 dark:text-white"> Press here to go back home.</RouterLink>
+
+    <RouterLink to="/" class="hover:text-green-700 dark:text-white">
+      Press here to go back home.</RouterLink
+    >
     <Footer></Footer>
   </div>
 </template>
