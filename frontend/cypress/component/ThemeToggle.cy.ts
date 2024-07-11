e88531f6da5d8755467576daa0f348b2b82485fa
Bobotato <bhojwanialexander@gmail.com>
2024-07-11 17:45:29 +0800

    feat: Remove component tests

--- frontend/cypress/component/ThemeToggle.cy.ts
+++
@@ -1,8 +0,0 @@
-import ThemeToggle from './ThemeToggle.vue'
-
-describe('<ThemeToggle />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-vue
-    cy.mount(ThemeToggle)
-  })
-})
