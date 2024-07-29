cfddf268685fce4e8b0e6de0274ea47d2a70765d
Bobotato <bhojwanialexander@gmail.com>
2024-07-29 22:55:36 +0800

    feat: Add accessibility tags for toggle dark mode button

--- frontend/src/components/utils/footer/ThemeToggle.vue
+++ frontend/src/components/utils/footer/ThemeToggle.vue
@@ -19,9 +19,9 @@ onMounted(() => {
 
 <template>
 <div class="flex flex-row gap-2">
-  <button id="theme-toggle" type="button" class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5" @click="toggleDark()">
-    <img id="theme-toggle-light-icon" src="/assets/BWIcons/sunIcon.svg" class="h-6 w-6" v-if="isDark"/>
-    <img id="theme-toggle-light-icon" src="/assets/BWIcons/moonIcon.svg" class="h-6 w-6" v-else/>
+  <button id="theme-toggle" type="button" aria-label="Toggle dark mode" class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5" @click="toggleDark()">
+    <img id="theme-toggle-light-icon" alt="Toggle light mode button" src="/assets/BWIcons/sunIcon.svg" class="h-6 w-6" v-if="isDark"/>
+    <img id="theme-toggle-dark-icon" alt="Toggle dark mode button" src="/assets/BWIcons/moonIcon.svg" class="h-6 w-6" v-else/>
   </button>
 </div>
 </template>
